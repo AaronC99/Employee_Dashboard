77127e5129a30f7391edb1167689e6ea1d1684d1
Aaron Chuah Hou Ern (HLB) <taaron@internal.hlb>
2019-02-19 15:39:41 +0800

    updated project nodejs

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+var http = require('http');
+var port = 8000;
+
+app.use(express.static(__dirname + '/web/dist/CoEEATS'));
+
+var httpServer = http.createServer(app);
+
+httpServer.listen(port, (err) => {
+
+	if (err) {
+		return console.log ('error', err)
+	}
+	console.log('server is alive port '+ port);
+});
